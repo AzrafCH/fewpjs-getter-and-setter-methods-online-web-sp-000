9826fb892d77d9657901ca459be535d339b024d3
Azraf Chowdhury <achowdhur@ltu.edu>
2020-09-06 10:22:12 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Add your Circle class here
+
+class Circle {
+  constructor(radius) {
+    this.radius = radius
+  }
+
+  get diameter() {
+    this.radius * 2
+  }
+
+  get circumference() {
+    Math.pi * diameter()
+  }
+}
